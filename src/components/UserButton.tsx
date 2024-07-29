0b49dc498e3de0bb143e2dd841116d83f71f360e
Trang Vu <106600297+Trangvu801@users.noreply.github.com>
2024-07-28 20:00:04 -0400

    11-ReactQuery

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -1,9 +1,10 @@
 "use client";
-
 import { logout } from "@/app/(auth)/actions";
 import { useSession } from "@/app/(main)/SessionProvider";
 import { cn } from "@/lib/utils";
-import { Check, CheckCircle, LogOutIcon, Monitor, Moon, Sun, UserIcon } from "lucide-react";
+import { useQueryClient } from "@tanstack/react-query";
+import { Check, LogOutIcon, Monitor, Moon, Sun, UserIcon } from "lucide-react";
+import { useTheme } from "next-themes";
 import Link from "next/link";
 import {
   DropdownMenu,
@@ -18,16 +19,15 @@ import {
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import UserAvatar from "./UserAvatar";
-import {useTheme} from "next-themes";
-
 interface UserButtonProps {
   className?: string;
 }
-
 export default function UserButton({ className }: UserButtonProps) {
   const { user } = useSession();
 
-  const {theme, setTheme} = useTheme();
+  const { theme, setTheme } = useTheme();
+
+  const queryClient = useQueryClient();
 
   return (
     <DropdownMenu>
@@ -47,25 +47,25 @@ export default function UserButton({ className }: UserButtonProps) {
         </Link>
         <DropdownMenuSub>
           <DropdownMenuSubTrigger>
-            <Monitor className="mr-2 size-4"/>
+            <Monitor className="mr-2 size-4" />
             Theme
           </DropdownMenuSubTrigger>
           <DropdownMenuPortal>
             <DropdownMenuSubContent>
               <DropdownMenuItem onClick={() => setTheme("system")}>
-                <Monitor className="mr-2 size-4"/>
+                <Monitor className="mr-2 size-4" />
                 System default
-                {theme === "system" && <Check className = "ms-2 size-4"/>}
+                {theme === "system" && <Check className="ms-2 size-4" />}
               </DropdownMenuItem>
               <DropdownMenuItem onClick={() => setTheme("light")}>
-                <Sun className="mr-2 size-4"/>
+                <Sun className="mr-2 size-4" />
                 Light
-                {theme === "light" &&  <Check className = "ms-2 size-4"/>}
+                {theme === "light" && <Check className="ms-2 size-4" />}
               </DropdownMenuItem>
               <DropdownMenuItem onClick={() => setTheme("dark")}>
-                <Moon className="mr-2 size-4"/>
+                <Moon className="mr-2 size-4" />
                 Dark
-                {theme === "dark" &&  <Check className = "ms-2 size-4"/>}
+                {theme === "dark" && <Check className="ms-2 size-4" />}
               </DropdownMenuItem>
             </DropdownMenuSubContent>
           </DropdownMenuPortal>
@@ -73,6 +73,7 @@ export default function UserButton({ className }: UserButtonProps) {
         <DropdownMenuSeparator />
         <DropdownMenuItem
           onClick={() => {
+            queryClient.clear();
             logout();
           }}
         >
