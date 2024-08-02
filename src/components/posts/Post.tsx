a944157cfb8e9643fe2ec9141b7174f600d15a07
Trang Vu <106600297+Trangvu801@users.noreply.github.com>
2024-08-02 13:58:52 -0400

    27-Comment-feature
    Still need to add "tag@" feature also along with like if possible.

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -1,10 +1,15 @@
+
 "use client";
+
 import { useSession } from "@/app/(main)/SessionProvider";
 import { PostData } from "@/lib/types";
 import { cn, formatRelativeDate } from "@/lib/utils";
 import { Media } from "@prisma/client";
+import { MessageSquare } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import Comments from "../comments/Comments";
 import Linkify from "../Linkify";
 import UserAvatar from "../UserAvatar";
 import UserTooltip from "../UserTooltip";
@@ -15,8 +20,12 @@ import PostMoreButton from "./PostMoreButton";
 interface PostProps {
   post: PostData;
 }
+
 export default function Post({ post }: PostProps) {
   const { user } = useSession();
+
+  const [showComments, setShowComments] = useState(false);
+
   return (
     <article className="group/post space-y-3 rounded-2xl bg-card p-5 shadow-sm">
       <div className="flex justify-between gap-3">
@@ -59,13 +68,19 @@ export default function Post({ post }: PostProps) {
       )}
       <hr className="text-muted-foreground" />
       <div className="flex justify-between gap-5">
-        <LikeButton
-          postId={post.id}
-          initialState={{
-            likes: post._count.likes,
-            isLikedByUser: post.likes.some((like) => like.userId === user.id),
-          }}
-        />
+        <div className="flex items-center gap-5">
+          <LikeButton
+            postId={post.id}
+            initialState={{
+              likes: post._count.likes,
+              isLikedByUser: post.likes.some((like) => like.userId === user.id),
+            }}
+          />
+          <CommentButton
+            post={post}
+            onClick={() => setShowComments(!showComments)}
+          />
+        </div>
         <BookmarkButton
           postId={post.id}
           initialState={{
@@ -75,12 +90,15 @@ export default function Post({ post }: PostProps) {
           }}
         />
       </div>
+      {showComments && <Comments post={post} />}
     </article>
   );
 }
+
 interface MediaPreviewsProps {
   attachments: Media[];
 }
+
 function MediaPreviews({ attachments }: MediaPreviewsProps) {
   return (
     <div
@@ -95,9 +113,11 @@ function MediaPreviews({ attachments }: MediaPreviewsProps) {
     </div>
   );
 }
+
 interface MediaPreviewProps {
   media: Media;
 }
+
 function MediaPreview({ media }: MediaPreviewProps) {
   if (media.type === "IMAGE") {
     return (
@@ -110,6 +130,7 @@ function MediaPreview({ media }: MediaPreviewProps) {
       />
     );
   }
+
   if (media.type === "VIDEO") {
     return (
       <div>
@@ -121,5 +142,23 @@ function MediaPreview({ media }: MediaPreviewProps) {
       </div>
     );
   }
+
   return <p className="text-destructive">Unsupported media type</p>;
+}
+
+interface CommentButtonProps {
+  post: PostData;
+  onClick: () => void;
+}
+
+function CommentButton({ post, onClick }: CommentButtonProps) {
+  return (
+    <button onClick={onClick} className="flex items-center gap-2">
+      <MessageSquare className="size-5" />
+      <span className="text-sm font-medium tabular-nums">
+        {post._count.comments}{" "}
+        <span className="hidden sm:inline">comments</span>
+      </span>
+    </button>
+  );
 }
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,5 @@
 import { Prisma } from "@prisma/client";
+
 export function getUserDataSelect(loggedInUserId: string) {
   return {
     id: true,
@@ -23,9 +24,11 @@ export function getUserDataSelect(loggedInUserId: string) {
     },
   } satisfies Prisma.UserSelect;
 }
+
 export type UserData = Prisma.UserGetPayload<{
   select: ReturnType<typeof getUserDataSelect>;
 }>;
+
 export function getPostDataInclude(loggedInUserId: string) {
   return {
     user: {
@@ -51,21 +54,43 @@ export function getPostDataInclude(loggedInUserId: string) {
     _count: {
       select: {
         likes: true,
+        comments: true,
       },
     },
   } satisfies Prisma.PostInclude;
 }
+
 export type PostData = Prisma.PostGetPayload<{
   include: ReturnType<typeof getPostDataInclude>;
 }>;
+
 export interface PostsPage {
   posts: PostData[];
   nextCursor: string | null;
 }
+
+export function getCommentDataInclude(loggedInUserId: string) {
+  return {
+    user: {
+      select: getUserDataSelect(loggedInUserId),
+    },
+  } satisfies Prisma.CommentInclude;
+}
+
+export type CommentData = Prisma.CommentGetPayload<{
+  include: ReturnType<typeof getCommentDataInclude>;
+}>;
+
+export interface CommentsPage {
+  comments: CommentData[];
+  previousCursor: string | null;
+}
+
 export interface FollowerInfo {
   followers: number;
   isFollowedByUser: boolean;
 }
+
 export interface LikeInfo {
   likes: number;
   isLikedByUser: boolean;
