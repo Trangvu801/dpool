7d9f1f44f6cb5b1b341e70e6a380284b993a40db
Trang Vu <106600297+Trangvu801@users.noreply.github.com>
2024-08-02 15:04:07 -0400

    28-notification-feature
    need to add tag on this

--- src/app/(main)/MenuBar.tsx
+++ src/app/(main)/MenuBar.tsx
@@ -1,79 +1,88 @@
-import { Button } from "@/components/ui/button"
-import { Bell, Bookmark, BookOpen, Home, Mail } from "lucide-react"
-import Link from "next/link"
+
+import { validateRequest } from "@/auth";
+import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
+import { Bookmark, BookOpen, Home, Mail, SquarePen } from "lucide-react";
+import Link from "next/link";
+import NotificationsButton from "./NotificationsButton";
 
 interface MenuBarProps {
-    className?: string
+  className?: string;
 }
 
-export default function MenuBar({className}: MenuBarProps) {
-    return <div className={className}>
-        <Button 
+export default async function MenuBar({ className }: MenuBarProps) {
+  const { user } = await validateRequest();
+
+  if (!user) return null;
+
+  const unreadNotificationCount = await prisma.notification.count({
+    where: {
+      recipientId: user.id,
+      read: false,
+    },
+  });
+
+  return (
+    <div className={className}>
+      <Button
         variant="ghost"
         className="flex items-center justify-start gap-3"
         title="Home"
-        asChild>
-            <Link href="/">
-                <Home />
-                <span className="hidden lg:inline">Home</span>
-            </Link>
-        </Button>
-
-        <Button 
-        variant="ghost"
-        className="flex items-center justify-start gap-3"
-        title="Notifications"
-        asChild>
-            <Link href="/notification">
-                <Bell />
-                <span className="hidden lg:inline">Notifications</span>
-            </Link>
-        </Button>
-
-        <Button 
+        asChild
+      >
+        <Link href="/">
+          <Home />
+          <span className="hidden lg:inline">Home</span>
+        </Link>
+      </Button>
+      <NotificationsButton
+        initialState={{ unreadCount: unreadNotificationCount }}
+      />
+      <Button
         variant="ghost"
         className="flex items-center justify-start gap-3"
         title="Messages"
-        asChild>
-            <Link href="/messages">
-                <Mail />
-                <span className="hidden lg:inline">Messages</span>
-            </Link>
-        </Button>
-
-        <Button 
+        asChild
+      >
+        <Link href="/messages">
+          <Mail />
+          <span className="hidden lg:inline">Messages</span>
+        </Link>
+      </Button>
+      <Button
         variant="ghost"
         className="flex items-center justify-start gap-3"
         title="Bookmarks"
-        asChild>
-            <Link href="/bookmarks">
-                <Bookmark />
-                <span className="hidden lg:inline">Bookmarks</span>
-            </Link>
-        </Button>
-
-        <Button 
+        asChild
+      >
+        <Link href="/bookmarks">
+          <Bookmark />
+          <span className="hidden lg:inline">Bookmarks</span>
+        </Link>
+      </Button>
+      <Button
         variant="ghost"
         className="flex items-center justify-start gap-3"
         title="Guidelines"
-        asChild>
-            <Link href="/guidelines">
-                <BookOpen />
-                <span className="hidden lg:inline">Guidelines</span>
-            </Link>
-        </Button>
-
-        <Button 
+        asChild
+      >
+        <Link href="/guidelines">
+          <BookOpen />
+          <span className="hidden lg:inline">Guidelines</span>
+        </Link>
+      </Button>
+      <Button
         variant="ghost"
         className="flex items-center justify-start gap-3"
         title="Feedback"
-        asChild>
-            <Link href="/feedback">
-                <BookOpen />
-                <span className="hidden lg:inline">Feedback</span>
-            </Link>
-        </Button>
-
+        asChild
+      >
+        <Link href="/feedback">
+          <SquarePen />
+          <span className="hidden lg:inline">Feedback</span>
+        </Link>
+      </Button>
+      
     </div>
-
+  );
 }
